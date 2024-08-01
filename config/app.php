7675f46c7cdbec536f07d2aef8c40dc44d5aa56a
unknown <docar.decz@gmail.com>
2024-08-01 14:28:47 -0400

    agregacion de https y proxies

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://laravel-cfp-production.up.railway.app'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL'),
 
