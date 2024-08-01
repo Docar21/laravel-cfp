695b53407293342c8d8acd30d86767343056c021
unknown <docar.decz@gmail.com>
2024-08-01 14:11:11 -0400

    Cambio de Url

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://laravel-cfp-production.up.railway.app'),
 
     'asset_url' => env('ASSET_URL'),
 
